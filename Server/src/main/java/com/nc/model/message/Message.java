0df5669e3e85b22349466276db9f71d932d55a01
gorenkopk <gorenkopk@gmail.com>
2019-06-17 06:43:13 +0300

    Add Message Class

---
+++ Server/src/main/java/com/nc/model/message/Message.java
@@ -0,0 +1,171 @@
+package com.nc.model.message;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="messageID" type="{http://www.w3.org/2001/XMLSchema}long"/>
+ *         &lt;element name="status" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="type" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="from" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="to" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="body" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "messageID",
+    "status",
+    "type",
+    "from",
+    "to",
+    "body"
+})
+@XmlRootElement(name = "message")
+public class Message {
+
+    @XmlElement(required = true)
+    protected long messageID;
+    @XmlElement(required = true)
+    protected int status;
+    @XmlElement(required = true)
+    protected String type;
+    @XmlElement(required = true)
+    protected int from;
+    @XmlElement(required = true)
+    protected int to;
+    @XmlElement(required = true)
+    protected String body;
+
+    /**
+     * Gets the value of the messageID property.
+     * 
+     */
+    public long getMessageID() {
+        return messageID;
+    }
+
+    /**
+     * Sets the value of the messageID property.
+     * 
+     */
+    public void setMessageID(long value) {
+        this.messageID = value;
+    }
+
+    /**
+     * Gets the value of the status property.
+     * 
+     */
+    public int getStatus() {
+        return status;
+    }
+
+    /**
+     * Sets the value of the status property.
+     * 
+     */
+    public void setStatus(int value) {
+        this.status = value;
+    }
+
+    /**
+     * Gets the value of the type property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getType() {
+        return type;
+    }
+
+    /**
+     * Sets the value of the type property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setType(String value) {
+        this.type = value;
+    }
+
+    /**
+     * Gets the value of the from property.
+     * 
+     */
+    public int getFrom() {
+        return from;
+    }
+
+    /**
+     * Sets the value of the from property.
+     * 
+     */
+    public void setFrom(int value) {
+        this.from = value;
+    }
+
+    /**
+     * Gets the value of the to property.
+     * 
+     */
+    public int getTo() {
+        return to;
+    }
+
+    /**
+     * Sets the value of the to property.
+     * 
+     */
+    public void setTo(int value) {
+        this.to = value;
+    }
+
+    /**
+     * Gets the value of the body property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getBody() {
+        return body;
+    }
+
+    /**
+     * Sets the value of the body property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setBody(String value) {
+        this.body = value;
+    }
+
+}
---
+++ Server/src/main/java/com/nc/model/message/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://foobar.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.nc.model.message;
